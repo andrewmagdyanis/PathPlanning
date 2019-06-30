436ad3f1ab00ce0f8e4ab1a36df15d11f74bb151
Andrew <andrewmagdyanis@gmail.com>
2019-06-30 06:14:10 +0200

    editing and commenting

--- scripts/a_star.py
+++ scripts/a_star.py
@@ -39,13 +39,23 @@ yR3=0
 xR3_goal=0
 yR3_goal=0
 
+#pose of robot4:
+xR4=0
+yR4=0
+#goal of robot4:
+xR4_goal=0
+yR4_goal=0
+
 #pose of obstacle1:
 xO1=0
 yO1=0
 #pose of obstacle2:
 xO2=0
 yO2=0
+#define list_goal to send it to go to goal as int array not string:
 list_goal=[]
+#define robot id:
+ID=1
 #------------------------------------------------------------------------------#
 # create the publisher: (name it Route)
 Route= rospy.Publisher('route_Rob_to_move',String, queue_size=10)
@@ -66,18 +76,23 @@ class node:
         self.F = F
 
     def __lt__(self, other):
+        '''function to:'''
         return self.F < other.F
 
     def updateF(self, xEnd, yEnd):
+        ''' function to update F '''
         self.F = self.G + self.H(xEnd, yEnd) * 10
 
     def H(self, xEnd, yEnd):
+        ''' function to calculate the eculidian distance '''
         xd = xEnd - self.xPos
         yd = yEnd - self.yPos
         d = math.sqrt(xd*xd + yd*yd)
         return(d)
 
     def updateG(self, dirs, d):
+        ''' Function to update the goal
+        '''
         if dirs == 8 and d % 2 != 0:
             self.G = self.G + 14
         else:
@@ -94,6 +109,9 @@ class node:
     deltaY:  ??
 '''
 def pathPlanner(the_map, n, m, dirs, deltX, deltaY, xA, yA, xB, yB):
+    # we create 3 lists of lists initialized by zeros with dimension equals to
+    # n*m which is the whole map dimension using the [0]*n then repeate append
+    # the rows created m times using for loop
     closed_nodes = []
     open_nodes = []
     parent_dir = []
@@ -102,17 +120,23 @@ def pathPlanner(the_map, n, m, dirs, deltX, deltaY, xA, yA, xB, yB):
         closed_nodes.append(list(row))
         open_nodes.append(list(row))
         parent_dir.append(list(row))
-
+    #create a queue as list of lists for the heap:
     pq = [[], []]
+    #create an index value to access elements on the queue
     pqi = 0
-
+    #create a node from the class (node), and give it the start and goal points:
     nA = node(xA, yA, 0, 0)
     nA.updateF(xB, yB)
+    #push the object (nA) from class (node) in the heap variable which is
+    #one element from the queue
     heappush (pq[pqi], nA)
+    #put the value of F into the open_nodes with start point indecies:
     open_nodes[yA][xA] = nA.F
 
+    #make a loop repeated while the queue has elements:
     while len(pq[pqi]) > 0:
-
+        #extract the elements from the queue on by one and use it to create new
+        #node using the class
         n1 = pq[pqi][0]
         nA = node(n1.xPos, n1.yPos, n1.G, n1.F)
         x = nA.xPos
@@ -170,87 +194,122 @@ def pathPlanner(the_map, n, m, dirs, deltX, deltaY, xA, yA, xB, yB):
 
 #------------------------------------------------------------------------------#
 ##set the callbacks function
-def callback1(data):# for pose of the robot to move
+def callback_pose_rob2move(data):# for pose of the robot to move
 	global xA,yA
 	xA=data.data[0]
 	yA=data.data[1]
-def callback2(data):# for goal of the robot to move
+def callback_goal_rob2move(data):# for goal of the robot to move
 	global xB,yB
 	xB=data.data[0]
 	yB=data.data[1]
+#another alternative to subscribe to the id of the robot to be moved:
+def callback_rob2move_id(data):
+    global ID
+    ID=data
+#we can also put the ID as constant or from the launch file to use it to choose
+#which pose and goal to be taken
+
 ####################################################
-def callback3(data):# for pose of robot1
+def callback_pose_rob1(data):# for pose of robot1
 	global xR1,yR1
 	xR1=data.data[0]
 	yR1=data.data[1]
-def callback4(data):# for goal of the robot1
+def callback_goal_rob1(data):# for goal of the robot1
 	global xR1_goal,yR1_goal
 	xR1_goal=data.data[0]
 	yR1_goal=data.data[1]
 ####################################################
-def callback5(data):# for pose of robot2
+def callback_pose_rob2(data):# for pose of robot2
 	global xR2,yR2
 	xR2=data.data[0]
 	yR2=data.data[1]
-def callback6(data):# for goal of the robot2
+def callback_goal_rob2(data):# for goal of the robot2
 	global xR2_goal,yR2_goal
 	xR2_goal=data.data[0]
 	yR2_goal=data.data[1]
 ####################################################
-def callback7(data):# for pose of robot3
+def callback_pose_rob3(data):# for pose of robot3
 	global xR3,yR3
 	xR3=data.data[0]
 	yR3=data.data[1]
-def callback8(data):# for goal of the robot3
+def callback_goal_rob3(data):# for goal of the robot3
 	global xR3_goal,yR3_goal
 	xR3_goal=data.data[0]
 	yR3_goal=data.data[1]
 ####################################################
-def callback9(data):# for pose of obstacle 1
+def callback_pose_rob4(data):# for pose of robot4
+	global xR4,yR4
+	xR4=data.data[0]
+	yR4=data.data[1]
+def callback_goal_rob3(data):# for goal of the robot4
+	global xR4_goal,yR4_goal
+	xR4_goal=data.data[0]
+	yR4_goal=data.data[1]
+####################################################
+def callback_pose_obs1(data):# for pose of obstacle 1
 	global xO1,yO1
 	xO1=data.data[0]
 	yO1=data.data[1]
-def callback10(data):# for pose of obstacle 2
+def callback_pose_obs2(data):# for pose of obstacle 2
 	global xO2,yO2
 	xO2=data.data[0]
 	yO2=data.data[1]
-
 	map_()
 
 #------------------------------------------------------------------------------#
 def listener():
     rospy.init_node('robot_map')
+    #subscribe to the Robot_id:
+    rospy.Subscriber('rob2move_id' ,Int32MultiArray,callback_rob2move_id)
     # Subscribe to robto to move position from the over head cam
-    rospy.Subscriber('rob_to_move_pose_px' ,Int32MultiArray,callback1)
-    rospy.Subscriber('robot_to_move_goal_px' ,Int32MultiArray,callback2)
+    rospy.Subscriber('rob_to_move_pose_px' ,Int32MultiArray,callback_pose_rob2move)
+    rospy.Subscriber('robot_to_move_goal_px' ,Int32MultiArray,callback_goal_rob2move)
     # Subscribe to robot1 position from the over head cam
-    rospy.Subscriber('rob1_pose_px' ,Int32MultiArray,callback3)
-    rospy.Subscriber('robot1_goal_px' ,Int32MultiArray,callback4)
+    rospy.Subscriber('rob1_pose_px' ,Int32MultiArray,callback_pose_rob1)
+    rospy.Subscriber('robot1_goal_px' ,Int32MultiArray,callback_goal_rob1)
 	# Subscribe to robot2 position from the over head cam
-    rospy.Subscriber('rob2_pose_px' ,Int32MultiArray,callback5)
-    rospy.Subscriber('robot2_goal_px' ,Int32MultiArray,callback6)
+    rospy.Subscriber('rob2_pose_px' ,Int32MultiArray,callback_pose_rob2)
+    rospy.Subscriber('robot2_goal_px' ,Int32MultiArray,callback_goal_rob2)
     # Subscribe to robot3 position from the over head cam
-    rospy.Subscriber('rob3_pose_px' ,Int32MultiArray,callback7)
-    rospy.Subscriber('robot3_goal_px' ,Int32MultiArray,callback8)
+    rospy.Subscriber('rob3_pose_px' ,Int32MultiArray,callback_pose_rob3)
+    rospy.Subscriber('robot3_goal_px' ,Int32MultiArray,callback_goal_rob3)
+    # Subscribe to robot3 position from the over head cam
+    rospy.Subscriber('rob4_pose_px' ,Int32MultiArray,callback_pose_rob4)
+    rospy.Subscriber('robot4_goal_px' ,Int32MultiArray,callback_goal_rob4)
     # Subscribe to obstcales position from the over head cam
-    rospy.Subscriber('Ob1_pose_px',Int32MultiArray,callback9)
-    rospy.Subscriber('Ob2_pose_px',Int32MultiArray,callback10)
+    rospy.Subscriber('Ob1_pose_px',Int32MultiArray,callback_pose_obs1)
+    rospy.Subscriber('Ob2_pose_px',Int32MultiArray,callback_pose_obs2)
 
 #------------------------------------------------------------------------------#
 def map_():
-	if((xB>0) or (yB>0)):
+    #get the goal and start point depeding on the id of the robot:
+    xA_list=[xR1,xR2,xR3,xR4]
+    yA_list=[yR1,yR2,yR3,yR4]
+    xA = xA_list[ID-1]
+    yA = yA_list[ID-1]
+
+    xB_list=[xR1_goal,xR2_goal,xR3_goal,xR4_goal]
+    yB_list=[yR1_goal,yR2_goal,yR3_goal,yR4_goal]
+    xB = xB_list[ID-1]
+    yB = yB_list[ID-1]
+
+    if((xB>0) or (yB>0)):
 		global dx,dy,list_goal
 		#set our possible directions (ups and downs and diagonals)
 		dirs = 8
+        # Cosines 45,90,135,180,225,270,315
 		dx = [1, 1, 0, -1, -1, -1, 0, 1]
-		dy = [0, 1, 1, 1, 0, -1, -1, -1]
-		#set our map dimension
-		n=10
-		m=10
-		the_map = []
-		row = [0] *n
-		for i in range(m):
-			the_map.append(list(row))
+        # Sines 45,90,135,180,225,270,315
+        dy = [0, 1, 1, 1, 0, -1, -1, -1]
+        #set our map dimension
+        n=10
+        m=10
+        #create a list of list by the dimension of n*m
+        the_map = []
+        row = [0] *n
+
+        for i in range(m):
+            the_map.append(list(row))
 
 		#xA = xA
 		#yA = yA
@@ -529,9 +588,9 @@ def map_():
 
         # else:
     	# 	print'waiting robot to move goal'
-        # ###
         pub_path_int.publish(Int32MultiArray(data= list_goal))
         time.sleep(3)
+
 
 
 #------------------------------------------------------------------------------#
